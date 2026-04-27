f2f6bbdee4bd9315889ca29fd9145f51d9b6e4af
Sebas.tian28 <92047249+Dev-Sebastian28@users.noreply.github.com>
2026-04-27 14:56:56 -0500

    [feat] Craete Secrets file and fix appConstants

--- Punto/Shared/Secrets.swift
+++ Punto/Shared/Secrets.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+struct Secrets {
+    static let baseURL = "https://burwzmykabjsyfkejqpk.supabase.co"
+    static let projectAPIKey = "sb_publishable_mapBlLho3ag4S8O_bTfHmw_o6miN0_U"
+
+}
--- Punto/Shared/appConstants.swift
+++ Punto/Shared/appConstants.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 struct AppConstants {
-    static let baseURL = "https://burwzmykabjsyfkejqpk.supabase.co"
-    static let projectAPIKey = "sb_publishable_gIc1E_HIXPkPN5T43zBCQw_L3Yo360M "
+    static let baseURL = Secrets.baseURL
+    static let projectAPIKey = Secrets.projectAPIKey
 }
+
